a9959c81f7f3fcaa8249ff6e75faecf09706939d
mahmoudyousuf <34246285+mahmoudyousuf@users.noreply.github.com>
2017-12-04 19:01:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mahmo/myloginapp/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.example.mahmo.myloginapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main2);
+         // EditText tx=(EditText)findViewById(R.id.edittext3);
+        // Bundle b=getIntent().getExtras();
+         //String data=b.getString("data");
+         //tx.setText("user id = "+data);
+
+    }
+}
